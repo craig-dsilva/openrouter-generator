0c9cfa21631faa3706df6f26a2b5205b622d5dfa
Craig D'Silva <craig.dsilva10@gmail.com>
2026-05-12 18:44:46 +0100

    Add Model dropdown

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from "react";
 import Layout from "./components/Layout/Layout";
 import "./App.css";
 
 const App = () => {
+  const [models, setModels] = useState<any>();
+
+  const fetchModels = async () => {
+    try {
+      const res = await fetch(
+        "https://openrouter.ai/api/v1/models?output_modalities=image",
+      );
+      const data = await res.json();
+      setModels(data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchModels();
+  }, []);
+
   return (
     <>
       <Layout>
+        <select name="model-selector" id="model-selector">
+          <option value="default">Select a model...</option>
+          {models &&
+            models.map((model: any) => {
+              return <option id={model.id}>{model.name}</option>;
+            })}
+        </select>
         <div className="main-prompt-container">
           <textarea
             className="main-prompt-textbox"
