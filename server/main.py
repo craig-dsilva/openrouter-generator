c0bc3acc445966a101be63a1b53ecba5f09e65ca
Craig D'Silva <craigdsilva@proton.me>
2026-09-11 13:06:09 +0100

    Host client on server

--- server/main.py
+++ server/main.py
@@ -1,9 +1,11 @@
 import requests
 import base64
 import datetime
+import os
 from fastapi import FastAPI, Header, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from pydantic import BaseModel, Field
 from typing import Annotated
 from pathlib import Path
@@ -13,6 +15,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
 ]
+
+static_dir = os.path.join(os.path.dirname(__file__), "../client/dist")
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,15 +31,22 @@ async def get_user_api_key(x_user_api_key: str = Header(...)):
         raise HTTPException(status_code=401, detail="API key required")
     return x_user_api_key
 
+# Serve the web page
+app.mount("/assets", StaticFiles(directory=os.path.join(static_dir, "assets")), name="assets")
+
 # Mounts image directory as route
 app.mount("/images", StaticFiles(directory="images"), name="images")
+app.mount("/", StaticFiles(directory="../client/dist", html=True), name="client")
 
 NonEmptyStr = Annotated[str, Field(min_length=1)]
 
 class Prompt(BaseModel):
     model: NonEmptyStr
     message: NonEmptyStr
-    
+
+@app.get("/{full_path:path}")
+def serve_react(full_path: str):
+    return FileResponse(os.path.join(static_dir, "index.html"))    
 
 @app.post("/image", status_code=200)
 async def image(prompt: Prompt, API_KEY_REF: str = Depends(get_user_api_key)):
